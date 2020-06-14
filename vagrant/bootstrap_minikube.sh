7054415fb7448662af641fdc3b2e91d482d791c0
MitchellGerdisch <github@jesslex.com>
2020-06-14 15:49:07 -0500

    testing

--- vagrant/bootstrap_minikube.sh
+++ vagrant/bootstrap_minikube.sh
@@ -5,33 +5,33 @@
 # It sets things up so minikube uses the docker driver so that minikube will run on  VM.
 #####
 
-sudo apt update > /dev/null
+sudo apt update 
 
-### start docker installation/setup ###
+### docker installation/setup ###
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common > /dev/null
+    software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable" > /dev/null
+   stable" 
 
-sudo apt-get update > /dev/null
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null
+sudo apt-get update 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
 
 # Add me as a privileged docker user
 sudo usermod -aG docker $USER && newgrp docker
 
 ## end docker installation/setup 
 
-### start minikube installation ###
+### minikube installation ###
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
 chmod +x minikube
 sudo mv minikube /usr/bin
 
@@ -41,8 +41,8 @@ minikube config set cpus 1
 
 ## end minikube installation
 
-### start install kubectl ###
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl > /dev/null
+### install kubectl ###
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl 
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/bin/kubectl
 ## end kubectl install
@@ -52,7 +52,6 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm init
-
 
 # Start up minikube cluster
 minikube start 
