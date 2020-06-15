ccf206031cff71ace61af2cedfb4eac3ad496093
MitchellGerdisch <github@jesslex.com>
2020-06-14 22:47:41 -0500

    more work

--- vagrant/bootstrap_k3s.sh
+++ vagrant/bootstrap_k3s.sh
@@ -8,16 +8,10 @@ curl -sfL https://get.k3s.io | sh -
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> $HOME/.profile
 
-echo "***** INSTALLING helm"
+echo "***** INSTALLING helm 3"
 ### 
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get 1> get_helm.sh 2> /dev/null
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh 2> /dev/null
-helm init
-
-echo "***** Setting up tiller user for helm."
-kubectl --namespace kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade
 
 echo "***** VM, ${HOSTNAME} k3s provisioning completed"
